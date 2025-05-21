447321a8a81b5ffbd82add19294629460fcce223
elhenryford <henry.ford@estudiantes.utec.edu.uy>
2025-05-21 17:45:25 -0300

    cambie los metodos por eventos

--- src/main/java/org/taller/moduloDeMonitoreo/interfaces/ServicioMonitoreoRemoto.java
+++
@@ -1,9 +0,0 @@
-package org.taller.moduloDeMonitoreo.interfaces;
-
-public interface ServicioMonitoreoRemoto {
-    void notificarPago();
-    void notificarPagoOk();
-    void notificarPagoError();
-    void notificarTransferencia();
-    void notificarReclamoComercio();
-}
